f834d0b2e4e83547fadaafe296a631d209fe164f
asm-jaime <asm.jaime@gmail.com>
2020-11-15 10:34:38 +1000

    limit: fix function

--- math/limit.js
+++ math/limit.js
@@ -1,9 +1,9 @@
-/* #minimax #limit #clamp
+/* #minmax #limit #clamp
  * task: limit a value in minimum and maximum limitations
  * >> 6, 10, 5
  * << 6
 */
 
 export default function limit (min, max, value) {
-  Math.min(Math.max(value, min), max);
+  return Math.min(Math.max(value, min), max);
 }
